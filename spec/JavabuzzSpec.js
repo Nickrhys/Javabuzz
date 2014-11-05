b28edd2997dbba16a19725f345e1b89efbdd4665
Nickrhys <nickrhys@hotmail.com>
2014-11-05 17:31:49 +0000

    passed not divisable by three test

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -8,6 +8,16 @@ describe('Javabuzz', function() {
       javabuzz = new Javabuzz();
       expect(javabuzz.isDivisableByThree(3)).toEqual(true);
     });
+  
+  });
+
+
+   describe('knows when a number is not', function(){ 
+
+    it('not divisable by three', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisableByThree(1)).toEqual(false);
+    });
 
   });
 
